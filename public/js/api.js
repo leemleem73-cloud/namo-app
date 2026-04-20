86a0060ed1c5c954779d325130587d7d0bb2d795
leemleem73-cloud <leemleem73@gmail.com>
2026-04-20 13:22:30 +0900

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,1 +1,14 @@
+async function api(url, options = {}) {
+  const res = await fetch(url, {
+    headers: { 'Content-Type': 'application/json', ...(options.headers || {}) },
+    ...options
+  });
 
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error || 'API 오류');
+  }
+
+  return data;
+}
