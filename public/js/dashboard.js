eae535b3249fe8ab7a7fc9c565bf646a9d5cb0bf
leemleem73-cloud <leemleem73@gmail.com>
2026-04-20 13:23:02 +0900

    Add clock display functionality to dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,21 @@
+function startClock() {
+  setInterval(() => {
+    const d = new Date();
+    const day = ['일','월','화','수','목','금','토'];
 
+    const text =
+      d.getFullYear() + '-' +
+      (d.getMonth()+1) + '-' +
+      d.getDate() + ' ' +
+      d.getHours() + ':' +
+      d.getMinutes() + ':' +
+      d.getSeconds() +
+      '(' + day[d.getDay()] + ')';
+
+    const el = document.getElementById('clock');
+    if (el) el.innerText = text;
+
+  }, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', startClock);
