ecd019567716ef090287822735e1847b6a12c866
leemleem73-cloud <leemleem73@gmail.com>
2026-08-05 13:47:39 +0900

    거래처 표와 부적합·설비 간격 보정 스크립트 로드

--- public/js/lot-shipment-status-sync.js
+++ public/js/lot-shipment-status-sync.js
@@ -21,6 +21,13 @@
     script.dataset.qmesNcrSeparateActionButtons="true";
     document.head.appendChild(script);
   }
+  if(!document.querySelector('script[data-qmes-table-equipment-spacing-20260805]')){
+    const script=document.createElement("script");
+    script.src="./js/table-and-equipment-spacing-20260805.js?v=20260805-1";
+    script.async=false;
+    script.dataset.qmesTableEquipmentSpacing20260805="true";
+    document.head.appendChild(script);
+  }
 })();
 
 (function(){
