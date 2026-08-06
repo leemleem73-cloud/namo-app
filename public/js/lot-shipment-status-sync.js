3cb0aa8b7a3dbf6420f85b14df482224903a8d4b
leemleem73-cloud <leemleem73@gmail.com>
2026-08-06 14:39:04 +0900

    왼쪽 메뉴·부적합·설비 보정 기능 복구

--- public/js/lot-shipment-status-sync.js
+++ public/js/lot-shipment-status-sync.js
@@ -12,15 +12,12 @@
   load('script[data-qmes-ui-refinement-20260805]',"./js/qmes-ui-refinement-20260805.js?v=20260805-5","qmesUiRefinement20260805");
   load('script[data-qmes-lot-detail-alignment-20260805]',"./js/lot-detail-alignment-20260805.js?v=20260806-31","qmesLotDetailAlignment20260805");
   load('script[data-qmes-iqc-persistence-pagination-fix]',"./js/iqc-persistence-pagination-fix-20260806.js?v=20260806-6","qmesIqcPersistencePaginationFix");
+  load('script[data-qmes-collapsible-side-menu]',"./js/qmes-collapsible-side-menu.js?v=20260806-7","qmesCollapsibleSideMenu");
+  load('script[data-qmes-ncr-separate-action-buttons]',"./js/ncr-separate-action-buttons.js?v=20260806-2","qmesNcrSeparateActionButtons");
   load('script[data-qmes-table-equipment-spacing-20260805]',"./js/table-and-equipment-spacing-20260805.js?v=20260805-10","qmesTableEquipmentSpacing20260805");
   load('script[data-qmes-equipment-layout-refinement-20260805]',"./js/equipment-layout-refinement-20260805.js?v=20260805-4","qmesEquipmentLayoutRefinement20260805");
-
-  /* 충돌 복구: 아래 전역 클릭/DOM 보정 스크립트는 원본 기능을 막아 로드하지 않음
-     - qmes-collapsible-side-menu.js
-     - ncr-separate-action-buttons.js
-     - partner-equipment-fix-20260805.js
-     - equipment-complaint-summary-refinement-20260805.js
-     - partner-equipment-action-fix-20260806.js */
+  load('script[data-qmes-partner-equipment-fix-20260805]',"./js/partner-equipment-fix-20260805.js?v=20260806-5","qmesPartnerEquipmentFix20260805");
+  load('script[data-qmes-partner-equipment-action-fix]',"./js/partner-equipment-action-fix-20260806.js?v=20260806-2","qmesPartnerEquipmentActionFix");
 })();
 
 (function(){
