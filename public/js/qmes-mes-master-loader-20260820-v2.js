543283dc7752b9fd6c6ea93ac4396b8aa20d15bd
leemleem73-cloud <leemleem73@gmail.com>
2026-08-24 13:47:33 +0900

    Load latest downtime history edit support

--- public/js/qmes-mes-master-loader-20260820-v2.js
+++ public/js/qmes-mes-master-loader-20260820-v2.js
@@ -17,7 +17,7 @@
     "./js/ipad-pqc-lot-completion-label-20260824.js?v=20260824-2",
     "./js/workorder-status-save-align-20260824.js?v=20260824-4",
     "./js/workorder-management-actions-layout-fix-20260824.js?v=20260824-sidebar-fit1",
-    "./js/production-downtime-history-20260824.js?v=20260824-1",
+    "./js/production-downtime-history-20260824.js?v=20260824-edit2",
     "./js/qmes-side-search-topbar-enhancement.js?v=20260807-1",
     "./js/qmes-scroll-layer-guard-20260807.js?v=20260807-1",
     "./js/qmes-ncr-delete-completed-20260810.js?v=20260810-2",
