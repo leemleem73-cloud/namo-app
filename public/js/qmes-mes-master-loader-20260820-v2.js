48db0807a5174fe43571e4979e805aac93f26b20
leemleem73-cloud <leemleem73@gmail.com>
2026-08-24 16:07:33 +0900

    Load PQC LOT dedupe selector v8

--- public/js/qmes-mes-master-loader-20260820-v2.js
+++ public/js/qmes-mes-master-loader-20260820-v2.js
@@ -19,7 +19,7 @@
     "./js/production-worker-live-name-sync-20260824.js?v=20260824-1",
     "./js/inspection-remove-new-buttons-20260810.js?v=20260824-remove1",
     "./js/ipad-pqc-oqc-date-field-sanitize-20260824.js?v=20260824-1",
-    "./js/ipad-pqc-lot-completion-label-20260824.js?v=20260824-7",
+    "./js/ipad-pqc-lot-completion-label-20260824.js?v=20260824-8",
     "./js/workorder-status-save-align-20260824.js?v=20260824-4",
     "./js/workorder-management-actions-layout-fix-20260824.js?v=20260824-sidebar-fit1",
     "./js/qmes-side-search-topbar-enhancement.js?v=20260807-1",
