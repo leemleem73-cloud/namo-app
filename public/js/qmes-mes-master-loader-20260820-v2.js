d0f237a4600bc4d50dfea7415dcdc74f2a2f62ea
leemleem73-cloud <leemleem73@gmail.com>
2026-08-24 13:51:36 +0900

    Load IQC basic field layout patch

--- public/js/qmes-mes-master-loader-20260820-v2.js
+++ public/js/qmes-mes-master-loader-20260820-v2.js
@@ -15,6 +15,7 @@
     "./js/production-worker-normalize-delete-20260824.js?v=20260824-safe2",
     "./js/production-worker-live-name-sync-20260824.js?v=20260824-1",
     "./js/ipad-pqc-lot-completion-label-20260824.js?v=20260824-2",
+    "./js/ipad-iqc-basic-layout-20260824.js?v=20260824-1",
     "./js/workorder-status-save-align-20260824.js?v=20260824-4",
     "./js/workorder-management-actions-layout-fix-20260824.js?v=20260824-sidebar-fit1",
     "./js/production-downtime-history-20260824.js?v=20260824-edit2",
