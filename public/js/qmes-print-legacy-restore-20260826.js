b257d0534b428e75b873fa0c851e38e662398cbd
leemleem73-cloud <leemleem73@gmail.com>
2026-08-27 16:41:35 +0900

    Remove disabled legacy print restore stub

--- public/js/qmes-print-legacy-restore-20260826.js
+++
@@ -1,8 +0,0 @@
-/* QMES print legacy restore disabled.
- * The application must use each document's current preview/print implementation.
- * This file intentionally does not replace window.printDoc or mutate print DOM.
- */
-(function(global){
-  "use strict";
-  global.__QMES_GLOBAL_PRINT_RESTORE_20260826__=false;
-})(window);
